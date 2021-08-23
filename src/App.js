f11c1f6680dd7cc2f40db97c1dc7dbd07f56b7aa
LinhDQ <Linhquang61@gmail.com>
2021-08-24 01:14:13 +0700

    feat: fix UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+// import ReactDOM from "react-dom";
+import "antd/dist/antd.css";
+import "./index.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import Product from "./component/product.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+     <div className="demo">
+        <div
+          style={{
+            marginLeft: 100,
+            clear: "both",
+            whiteSpace: "nowrap",
+            display: "flex",
+          }}
+          className="products"
         >
-          Learn React
-        </a>
-      </header>
+          <Product key="1" />
+          <Product key="2" />
+          <Product key="3" />
+          <Product key="4" />
+          <Product key="5" />
+          <Product key="6" />
+          <Product key="7" />
+          <Product key="8" />
+          <Product key="9" />
+          <Product key="10" />
+          <Product key="11" />
+          <Product key="12" />
+          <Product key="13" />
+          <Product key="14" />
+          <Product key="15" />
+          <Product key="16" />
+          <Product key="17" />
+          <Product key="18" />
+          <Product key="19" />
+          <Product key="20" />
+          <Product key="21" />
+          <Product key="22" />
+          <Product key="23" />
+          <Product key="24" />
+          <Product key="25" />
+          <Product key="26" />
+          <Product key="27" />
+          <Product key="28" />
+          <Product key="29" />
+          <Product key="30" />
+          <Product key="31" />
+          <Product key="32" />
+        </div>
+      </div>
     </div>
   );
 }
